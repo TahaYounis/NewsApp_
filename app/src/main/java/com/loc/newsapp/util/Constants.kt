70264353d442d169c7f0ebb689a7cacc6922cf6a
TahaYounis <pro.tahayounis@gmail.com>
2024-02-10 20:56:18 +0200

    upload

---
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -0,0 +1,12 @@
+package com.loc.newsapp.util
+
+
+object Constants {
+
+    const val USER_SETTINGS = "user_settings"
+    const val APP_ENTRY = "app_entry"
+
+    const val BASE_URL = "https://newsapi.org/v2/"
+    const val API_KEY = "696cd46378f74b64b45a6c24763e6b64"
+
+}
